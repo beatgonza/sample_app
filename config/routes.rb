a4d3aaad7d5b69bb72a238adf191d9f487cc3fa9
Beatriz Gonzalez <beatgonza@gmail.com>
2012-04-23 11:40:13 -0700

    Finish layout and routes'

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
+
   root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
